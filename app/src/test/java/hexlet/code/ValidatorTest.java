abd95eea4a833603e4a545c562cbca0c624d651c
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-05-10 17:11:07 +0300

    fix checkstyle errors 2

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
